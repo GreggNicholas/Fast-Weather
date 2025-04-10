a40e88dadbdd9caf86b0386232fcaa51f927a3bb
GreggNicholas <greggnicholas@pursuit.org>
2025-04-10 14:53:48 -0400

    Create singleton object for weather api JSON

---
+++ app/src/main/java/com/example/fastweather/RetrofitClient.kt
@@ -0,0 +1,17 @@
+package com.example.fastweather
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.weatherapi.com/v1/"
+
+
+    val weatherAPIService: WeatherAPIService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(WeatherAPIService::class.java)
+    }
+}
