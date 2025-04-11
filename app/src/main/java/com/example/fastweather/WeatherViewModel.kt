681fb966cfaec2b5cf5f4ae66c27e4ab6c1c26aa
GreggNicholas <greggnicholas@pursuit.org>
2025-04-11 13:09:37 -0400

    Create WeatherViewModel for weather data

---
+++ app/src/main/java/com/example/fastweather/WeatherViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.fastweather
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+
+class WeatherViewModel:ViewModel() {
+    var weather by mutableStateOf<WeatherResponse?>(null)
+        private set
+    var errorMessage by mutableStateOf<String?>(null)
+
+    fun fetchWeather(city:String, apiKey:String) {
+        viewModelScope.launch {
+            try {
+                val response = RetrofitClient.weatherAPIService.getCurrentWeather(apiKey,city)
+            weather = response
+                errorMessage = null
+            } catch (e: Exception){
+                errorMessage = "Could not fetch weather: &{e.message}"
+            }
+        }
+
+    }
+}
