d325cc10974dbf0b1df55117b84d5c35c6af151a
GreggNicholas <greggnicholas@pursuit.org>
2025-04-12 20:14:12 -0400

    Create Composable for Weatherscreen

--- app/src/main/java/com/example/fastweather/MainActivity.kt
+++ app/src/main/java/com/example/fastweather/MainActivity.kt
@@ -18,15 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FastWeatherTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
-        }
     }
 }
 
