3b15dce8f9b2d66358561230452eaa923a3fc5c8
GreggNicholas <greggnicholas@pursuit.org>
2025-05-06 22:10:58 -0400

    Enable user to press enter to fetch city weather

--- app/src/main/java/com/example/fastweather/MainActivity.kt
+++ app/src/main/java/com/example/fastweather/MainActivity.kt
@@ -6,11 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.fastweather.ui.theme.FastWeatherTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -33,21 +28,5 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherScreen(viewModel = viewModel, apiKey = apiKey)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FastWeatherTheme {
-        Greeting("Android")
     }
 }
